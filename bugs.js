fea0d92ca53351497bccae51aae6cd368728808c
bryannasav <bryanna.savelesky@gmail.com>
2020-02-16 10:46:27 -0800

    initial upload

---
+++ bugs.js
@@ -0,0 +1,23 @@
+var firstName = "Robert";
+var nickName = "Bob";
+var petName = Fluffy;
+xar petType = "Turtle";
+if(name == "Robert"){
+    console.log("Hi Robert");
+}
+else if(name == "Bob"){
+    console("Hi Bob");
+}
+else(name != "Robert"){
+    console.log("Hello Stranger");
+}
+if petType == "Dog" {
+    console.log("Time to play fetch");
+}
+if(petType = "Cat"){
+    console.log("Time to play with a laser pointer");
+}
+else
+    console.log(Time to play with my pet);
+}
+
