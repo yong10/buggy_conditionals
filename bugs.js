fc4f7099b5b3ecb695d8c635d203b96516902bb4
Yong Noh <yh650766@gmail.com>
2020-02-18 13:46:19 -0500

    add

--- bugs.js
+++ bugs.js
@@ -1,23 +1,22 @@
 var firstName = "Robert";
 var nickName = "Bob";
-var petName = Fluffy;
-xar petType = "Turtle";
-if(name == "Robert"){
+var petName = "Fluffy";
+var petType = "Turtle";
+if(firstName = "Robert"){
     console.log("Hi Robert");
 }
-else if(name == "Bob"){
-    console("Hi Bob");
+if(nickName = "Bob"){
+    console.log("Hi Bob");
 }
-else(name != "Robert"){
+else{
     console.log("Hello Stranger");
 }
-if petType == "Dog" {
+if(petType == "Dog") {
     console.log("Time to play fetch");
 }
-if(petType = "Cat"){
+else if(petType == "Cat"){
     console.log("Time to play with a laser pointer");
 }
-else
-    console.log(Time to play with my pet);
+else{
+    console.log("Time to play with my pet");
 }
-
